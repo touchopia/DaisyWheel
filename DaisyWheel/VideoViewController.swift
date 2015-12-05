afa68f0089239bdc429e564794fa18deb5c07914
Phil Wright <touchopia@me.com>
2015-12-05 11:11:07 -0700

    Fixed Video

--- DaisyWheel/VideoViewController.swift
+++ DaisyWheel/VideoViewController.swift
@@ -2,17 +2,35 @@
 //  VideoViewController.swift
 //  DaisyWheel
 //
-//  Created by Phil Wright on 12/5/15.
+//  Created by Phil Wright on 12/1/15.
 //  Copyright © 2015 Touchopia, LLC. All rights reserved.
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class VideoViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationController?.navigationBarHidden = true
+        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue,
+        sender: AnyObject?) {
+            
+            if segue.identifier == "MoviePlayer" {
+                let destination = segue.destinationViewController as! AVPlayerViewController
+                
+                if let path = NSBundle.mainBundle().pathForResource("GITGirlsProgram", ofType:"mp4") {
+                    let url = NSURL(fileURLWithPath: path)
+                    destination.player = AVPlayer(URL: url)
+                }
+            }
+    }
 }
